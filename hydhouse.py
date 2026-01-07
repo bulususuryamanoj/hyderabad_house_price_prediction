dbab485871cf3398e72ec6d94676cef366c116ad
bulususuryamanoj <suryamanoj1210@gmail.com>
2026-01-07 14:32:59 +0530

    added redirecting the results to the webpage

--- hydhouse.py
+++ hydhouse.py
@@ -1,6 +1,7 @@
 import numpy
 from flask import Flask,render_template,request
 import pickle
+import pandas as pd
 app = Flask(__name__)
 app.name = 'Hyderabad House Price Prediction'
 
@@ -21,9 +22,21 @@ def predict():
         input_data = numpy.array([[title,location,area_insqft,building_status]])
         predictions = multi_xgb_model.predict(input_data)
         price, rate_persqft = predictions[0][0], predictions[0][1]
+        title_map_list = pd.read_csv(r"D:\python\hydhouse\title_mapping.csv")
+        title_map = title_map_list.loc[title_map_list['code'] == title, 'category'].iloc[0]
+        building_status_list = pd.read_csv(r"D:\python\hydhouse\building_status_mapping.csv")
+        building_status_re = building_status_list.loc[building_status_list['code']== building_status,'category'].iloc[0]
+        location_list = pd.read_csv(r"D:\python\hydhouse\location_mapping.csv")
+        location_re = location_list.loc[location_list['code']==location,'category'].iloc[0]
+
+
 
         return render_template(
             "result.html",
+            title_map = title_map,
+            location_re = location_re,
+            area_insqft = area_insqft,
+            building_status_re = building_status_re,
             prediction_price =f"Predicted Price: {price:.2f} Lakh",predicted_rate_per_sqft = f"Rate per sqft: {rate_persqft:.2f}"
         )
     except Exception as e:
